0eefa413ccc4f7e379d2a33a7a8d06a7e64d64a6
Christian Dirnhofer <christian@dirnhofer.net>
2020-12-09 10:21:41 +0100

    pushing multiple version when build as latest

--- hooks/getversion.sh
+++ hooks/getversion.sh
@@ -2,3 +2,29 @@ export RESPONSE=$(curl -sb -H "Accept: application/json" "https://api.github.com
 export SUBSELECT="`echo $(echo $RESPONSE | grep -oP '"tag_name":\s"zephyr-(v\d\.\d\.\d)"[\d|\D]+"draft":\s(\w+),')`"
 export VERSION="`echo $SUBSELECT | grep -oP '"tag_name":\s"zephyr-\K(v\d\.\d\.\d)'`"
 export IS_DRAFT="`echo $SUBSELECT | grep -oP '"draft":\s\K(\w+)'`"
+
+VERSIONS=$(echo $VERSION | tr "." "\n")
+
+COUNTER=0
+for sub in $VERSIONS
+do
+    case $COUNTER  in
+        0)
+            export MAJOR=$sub
+            ;;
+
+        1)
+            export MINOR=$sub
+            ;;
+
+        2)
+            export PATCH=$sub
+            ;;
+
+        *)
+            echo -n "unknown"
+            ;;
+    esac
+
+    ((COUNTER=COUNTER+1))
+done
